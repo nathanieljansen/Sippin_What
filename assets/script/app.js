bb7c41ed43cbe4f7f55233690bfbc76a8369e7b4
Nathaniel Jansen <nathanieljansen@Nathaniels-MacBook-Pro.local>
2018-03-17 14:44:47 -0600

    text box changes

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,11 +1,8 @@
-
-var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=steak&maxPrice=50"
-
 $(".btn").click(function () {
   var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=chicken&maxPrice=50",
+    "url": "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=alfredo&maxPrice=50",
     "method": "GET",
     "headers": {
       "X-Mashape-Key": "aVuMKS8FG3mshVQlO5dNdPxZQCdrp1FpzUDjsnZtHrg9bA3DEP",
