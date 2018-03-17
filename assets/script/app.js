2ea12b62812035258fc693172b09b458ded32817
Nathaniel Jansen <nathanieljansen@Nathaniels-MacBook-Pro.local>
2018-03-17 16:01:57 -0600

    changes made

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,16 +1,22 @@
-$(".btn").click(function () {
-  var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=alfredo&maxPrice=50",
-    "method": "GET",
-    "headers": {
-      "X-Mashape-Key": "aVuMKS8FG3mshVQlO5dNdPxZQCdrp1FpzUDjsnZtHrg9bA3DEP",
-      "Cache-Control": "no-cache",
-    }
-  }
+// var foodInput = $("");
+// var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + foodInput + "&maxPrice=50";
 
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  })
+$(function () {
+$(".btn").click(function () {
+  // console.log(foodInput)
+  // var settings = {
+  //   "async": true,
+  //   "crossDomain": true,
+  //   "url": queryURL,
+  //   "method": "GET",
+  //   "headers": {
+  //     "X-Mashape-Key": "aVuMKS8FG3mshVQlO5dNdPxZQCdrp1FpzUDjsnZtHrg9bA3DEP",
+  //     "Cache-Control": "no-cache",
+  //   }
+  // }
+
+  // $.ajax(settings).done(function (response) {
+  //   console.log(response);
+  // })
+})
 });
