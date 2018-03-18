abadd7fe482823f50617ad8ef702f67620b2213b
Nathaniel Jansen <nathanieljansen@Nathaniels-MacBook-Pro.local>
2018-03-18 10:30:08 -0600

    got responses to work based on input

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,20 +1,22 @@
-
-var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=steak&maxPrice=50"
+$(function () {
 
 $(".btn").click(function () {
+  var textInput = $(".autocomplete").val().trim().toUpperCase();
+  var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + textInput + "&maxPrice=50";
   var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=steak&maxPrice=50",
+    "url": queryURL,
     "method": "GET",
     "headers": {
       "X-Mashape-Key": "aVuMKS8FG3mshVQlO5dNdPxZQCdrp1FpzUDjsnZtHrg9bA3DEP",
       "Cache-Control": "no-cache",
-
     }
   }
 
   $.ajax(settings).done(function (response) {
     console.log(response);
   })
+  $(".autocomplete").val("");
+})
 });
