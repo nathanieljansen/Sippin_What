c08a0fa88b49e49d6802c96fe4f3b83dac39e2b3
Nathaniel Jansen <nathanieljansen@Nathaniels-MacBook-Pro.local>
2018-03-18 10:06:06 -0600

    css changes to the textboxt

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,22 +1,20 @@
-// var foodInput = $("");
-// var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + foodInput + "&maxPrice=50";
 
-$(function () {
+var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=steak&maxPrice=50"
+
 $(".btn").click(function () {
-  // console.log(foodInput)
-  // var settings = {
-  //   "async": true,
-  //   "crossDomain": true,
-  //   "url": queryURL,
-  //   "method": "GET",
-  //   "headers": {
-  //     "X-Mashape-Key": "aVuMKS8FG3mshVQlO5dNdPxZQCdrp1FpzUDjsnZtHrg9bA3DEP",
-  //     "Cache-Control": "no-cache",
-  //   }
-  // }
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=steak&maxPrice=50",
+    "method": "GET",
+    "headers": {
+      "X-Mashape-Key": "aVuMKS8FG3mshVQlO5dNdPxZQCdrp1FpzUDjsnZtHrg9bA3DEP",
+      "Cache-Control": "no-cache",
 
-  // $.ajax(settings).done(function (response) {
-  //   console.log(response);
-  // })
-})
+    }
+  }
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  })
 });
