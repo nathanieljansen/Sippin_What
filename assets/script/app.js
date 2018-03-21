4d3ade9943582f0ba53b1cfc27ed7519c64e66fc
Nathaniel Jansen <nathanieljansen@Nathaniels-MacBook-Pro.local>
2018-03-20 19:36:48 -0600

    not sure what changed

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,22 +1,22 @@
 $(function () {
 
-$(".btn").click(function () {
-  var textInput = $(".autocomplete").val().trim().toUpperCase();
-  var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + textInput + "&maxPrice=50";
-  var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": queryURL,
-    "method": "GET",
-    "headers": {
-      "X-Mashape-Key": "aVuMKS8FG3mshVQlO5dNdPxZQCdrp1FpzUDjsnZtHrg9bA3DEP",
-      "Cache-Control": "no-cache",
+  $(".btn").click(function () {
+    var textInput = $(".autocomplete").val().trim().toUpperCase();
+    var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + textInput + "&maxPrice=50";
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": queryURL,
+      "method": "GET",
+      "headers": {
+        "X-Mashape-Key": "aVuMKS8FG3mshVQlO5dNdPxZQCdrp1FpzUDjsnZtHrg9bA3DEP",
+        "Cache-Control": "no-cache",
+      }
     }
-  }
 
-  $.ajax(settings).done(function (response) {
-    console.log(response);
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    })
+    $(".autocomplete").val("");
   })
-  $(".autocomplete").val("");
-})
 });
