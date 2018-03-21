0411dfb5bf2a30e4d28e5e52d40d59510191fd42
ckgibb61 <giblinck@gmail.com>
2018-03-21 16:40:01 -0600

    Add files via upload

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,46 +1,47 @@
 $(function () {
 
-  var config = {
-    apiKey: "AIzaSyCEsZxf-oF64tfJK_saGz85n0TqBi1yHlA",
-    authDomain: "sippin-what.firebaseapp.com",
-    databaseURL: "https://sippin-what.firebaseio.com",
-    projectId: "sippin-what",
-    storageBucket: "sippin-what.appspot.com",
-    messagingSenderId: "175010578796"
-  };
-  firebase.initializeApp(config);
-
-  var database = firebase.database();
-
-  $(".btn").click(function () {
-    event.preventDefault();
-    var textInput = $(".autocomplete").val().trim().toLowerCase();
-    var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + textInput + "&maxPrice=50";
-    var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": queryURL,
-      "method": "GET",
-      "headers": {
-        "X-Mashape-Key": "aVuMKS8FG3mshVQlO5dNdPxZQCdrp1FpzUDjsnZtHrg9bA3DEP",
-        "Cache-Control": "no-cache",
-      }
+$(".btn").click(function () {
+  var textInput = $(".autocomplete").val().trim().toUpperCase();
+  var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + textInput + "&maxPrice=50";
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": queryURL,
+    "method": "GET",
+    "headers": {
+      "X-Mashape-Key": "aVuMKS8FG3mshVQlO5dNdPxZQCdrp1FpzUDjsnZtHrg9bA3DEP",
+      "Cache-Control": "no-cache",
+      "Origin": null
     }
-    $.ajax(settings).then(function (response) {
-      console.log(response);
-        if (response.status == "failure") {
-          console.log(response.message)
-        }
-      })
-      
-    var newFood = {
-      newFood: textInput,
-    }
-      database.ref().push(newFood);
-      $(".autocomplete").val("");
-    });
-    database.ref().on("child_added", function (childSnapshot, prevChildKey) {
-      console.log(childSnapshot.val());
-    });
-  });
+  }
 
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    var p = $("<p>");
+    p.text(response.pairingText);
+    $("#words").append(p);
+
+    var description = $("<p>");
+    description.text(response.productMatches[0].description);
+    $("#words").append(description);
+    
+    var img = $("<img>");
+    img.attr("src", response.productMatches[0].imageUrl);
+    $("#image").append(img);
+
+    var title = $("<p>");
+    title.text(response.productMatches[0].title)
+    $("#image").append(title);
+
+
+///if else statement for no wine found v. wine 
+// if response.status append error else run normally?
+
+  })
+  $(".autocomplete").val("");
+
+
+
+});
+
+});
