fb93353018398d67c4907f51f4cde8d03895aafe
ckgibb61 <giblinck@gmail.com>
2018-03-21 20:40:40 -0600

    append to page

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,69 +1,47 @@
 $(function () {
 
-  var config = {
-    apiKey: "AIzaSyCEsZxf-oF64tfJK_saGz85n0TqBi1yHlA",
-    authDomain: "sippin-what.firebaseapp.com",
-    databaseURL: "https://sippin-what.firebaseio.com",
-    projectId: "sippin-what",
-    storageBucket: "sippin-what.appspot.com",
-    messagingSenderId: "175010578796"
-  };
-  firebase.initializeApp(config);
+$(".btn").click(function () {
+  var textInput = $(".autocomplete").val().trim().toUpperCase();
+  var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + textInput + "&maxPrice=50";
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": queryURL,
+    "method": "GET",
+    "headers": {
+      "X-Mashape-Key": "aVuMKS8FG3mshVQlO5dNdPxZQCdrp1FpzUDjsnZtHrg9bA3DEP",
+      "Cache-Control": "no-cache",
+      "Origin": null
+    }
+  }
 
-  var database = firebase.database();
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    var p = $("<p>");
+    p.text(response.pairingText);
+    $("#words").append(p);
 
-  // $(".autocomplete").keyup(function (event) {
-  //   var letterinput = $(".autocomplete");
-  //   var autocompleteURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/autocomplete?number=10&query=" + letterinput;
-  //   var autocompleteAPI = {
-  //     "async": true,
-  //     "crossDomain": true,
-  //     "url": autocompleteURL,
-  //     "method": "GET",
-  //     "headers": {
-  //       "X-Mashape-Key": "aVuMKS8FG3mshVQlO5dNdPxZQCdrp1FpzUDjsnZtHrg9bA3DEP",
-  //       "Cache-Control": "no-cache",
-  //     }
-  //   }
-  //   $.ajax(autocompleteAPI).then(function (response) {
-  //     console.log(response)
-  //   })
-  // })
+    var description = $("<p>");
+    description.text(response.productMatches[0].description);
+    $("#words").append(description);
+    
+    var img = $("<img>");
+    img.attr("src", response.productMatches[0].imageUrl);
+    $("#image").append(img);
 
-    $(".btn").click(function () {
-      event.preventDefault();
-      var textInput = $(".autocomplete").val().trim().toLowerCase();
-      var wineQueryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + textInput + "&maxPrice=50";
-      var wineAPI = {
-        "async": true,
-        "crossDomain": true,
-        "url": wineQueryURL,
-        "method": "GET",
-        "headers": {
-          "X-Mashape-Key": "aVuMKS8FG3mshVQlO5dNdPxZQCdrp1FpzUDjsnZtHrg9bA3DEP",
-          "Cache-Control": "no-cache",
-        }
-      }
-      $.ajax(wineAPI).then(function (response) {
-        console.log(response)
-        if (response.status === "failure") {
-          $("#autocomplete-input").text(response.message)
-        }
-        else if (response.pairingText === "") {
-          $("#autocomplete-input").html("we don't have dat")
-        }
-        else $(".input-field").hide();
-        $(".mainHeader").html("WHAT YOU'RE SIPPIN'")
-      })
+    var title = $("<p>");
+    title.text(response.productMatches[0].title)
+    $("#image").append(title);
 
-      var newFood = {
-        newFood: textInput,
-      }
-      database.ref().push(newFood);
-      $(".autocomplete").val("");
-    });
-    database.ref().on("child_added", function (childSnapshot, prevChildKey) {
-      console.log(childSnapshot.val());
-    });
-  });
 
+///if else statement for no wine found v. wine 
+// if response.status append error else run normally?
+
+  })
+  $(".autocomplete").val("");
+
+
+
+});
+
+});
