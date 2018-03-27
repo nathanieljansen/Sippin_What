3e284a9b8f83bdbdff8602fde5443327d820ebdd
Carol Giblin <giblinCK@gmail.com>
2018-03-26 20:42:58 -0600

    reset & zip

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,10 +1,20 @@
 
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
+var config = {
+  apiKey: "AIzaSyCEsZxf-oF64tfJK_saGz85n0TqBi1yHlA",
+  authDomain: "sippin-what.firebaseapp.com",
+  databaseURL: "https://sippin-what.firebaseio.com",
+  projectId: "sippin-what",
+  storageBucket: "sippin-what.appspot.com",
+  messagingSenderId: "175010578796"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var zip = "";
+
 var map, infoWindow;
-$(".wineSwipe").hide();
+// $(".wineSwipe").hide();
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 39.7392, lng: -104.9903 },
@@ -17,13 +27,21 @@ function initMap() {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
+      console.log("its working", pos)
 
-      // var marker = new google.maps.Marker({
-      //   position: pos,
-      //   map: map,
-      //   title: 'Hello World!'
-      // })
+      $.ajax({
+        url: "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + pos.lat + "," + pos.lng + "&sensor=true",
+        type: "GET",
+      }).then(function (resultsBack) {
+        console.log('did it work ???', resultsBack.results[2].address_components[0].long_name);
+        zip = resultsBack.results[2].address_components[0].long_name;
+      })
       
+      // var userPostion = {
+      //   userPosition: pos,
+      // }
+      // database.ref().push(userPosition)
+
       infowindow = new google.maps.InfoWindow();
       var service = new google.maps.places.PlacesService(map);
       service.nearbySearch({
@@ -39,7 +57,7 @@ function initMap() {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
-  
+
 }
 
 function callback(results, status) {
@@ -69,28 +87,24 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     'Error: The Geolocation service failed.' :
     'Error: Your browser doesn\'t support geolocation.');
   infoWindow.open(map);
-  
+
 }
+
+
 $(function () {
   $('.parallax').parallax();
- 
 
-  var config = {
-    apiKey: "AIzaSyCEsZxf-oF64tfJK_saGz85n0TqBi1yHlA",
-    authDomain: "sippin-what.firebaseapp.com",
-    databaseURL: "https://sippin-what.firebaseio.com",
-    projectId: "sippin-what",
-    storageBucket: "sippin-what.appspot.com",
-    messagingSenderId: "175010578796"
-  };
-  firebase.initializeApp(config);
+  function pageScroll() {
+    window.scrollBy(0, 325);
+    // scrolldelay = setTimeout(pageScroll, 7);
+  }
 
-  var database = firebase.database();
+
 
 
   $(".btn").click(function () {
-    
     event.preventDefault();
+
     var textInput = $(".autocomplete").val().trim().toLowerCase();
     var wineQueryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + textInput + "&maxPrice=50";
     var wineAPI = {
@@ -106,47 +120,89 @@ $(function () {
     $.ajax(wineAPI).then(function (response) {
       console.log(response)
 
-      var p = $("<p>");
-      p.text(response.pairingText);
-      $("#words").append(p);
-
-      var description = $("<p>");
-      description.text(response.productMatches[0].description);
-      $("#words").append(description);
-
-      var img = $("<img>");
-      img.attr("src", response.productMatches[0].imageUrl);
-      $("#image").append(img);
-
-      var title = $("<p>");
-      title.text(response.productMatches[0].title)
-      $("#image").append(title);
-      $(".wineSwipe").show();
 
       if (response.status === "failure") {
-        $("#autocomplete-input").text(response.message)
+        console.log(response.message)
+        $(".notValid").text("Sorry! " + response.message + ". We are always trying to improve. Thanks for you help!")
       }
       else if (response.pairingText === "") {
-        $("#autocomplete-input").html("we don't have dat")
+        $(".notValid").text("Thanks for making us better! We didn't find a pairing for " + textInput + " but we are always trying to improve our app")
       }
 
+      else {
+        $(".notValid").empty();
+        pageScroll();
+        var otherWines = response.pairedWines[0];
 
-      $("#reset").on('click', function(){
-        $("#words").empty();
+       $("#words").empty();
+        var p = $("<p>");
+        p.text(response.pairingText);
+        $("#words").append(p);
+
+        var description = $("<p>");
+        description.text(response.productMatches[0].description);
+        $("#words").append(description);
+
         $("#image").empty();
-        $(".input-field").show();
-      });
+        console.log("empty again")
+        var img = $("<img>");
+        img.attr("src", response.productMatches[0].imageUrl);
+        $("#image").html(img);
+
+        var title = $("<p>");
+        title.text(response.productMatches[0].title)
+        $("#image").append(title);
+        $(".wineSwipe").show();
+      }
+
+      var comparableWineQueryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/recommendation?maxPrice=50&minRating=0.7&number=3&wine=" + otherWines;
+      var comparableAPI = {
+        "async": true,
+        "crossDomain": true,
+        "url": comparableWineQueryURL,
+        "method": "GET",
+        "headers": {
+          "X-Mashape-Key": "aVuMKS8FG3mshVQlO5dNdPxZQCdrp1FpzUDjsnZtHrg9bA3DEP",
+          "Cache-Control": "no-cache",
+        }
+      }
+      $.ajax(comparableAPI).then(function (response) {
+        console.log(response.recommendedWines)
+        $("#otherWineImage1").empty();
+        var img = $("<img>");
+        img.attr("src", response.recommendedWines[0].imageUrl);
+        $("#otherWineImage1").append(img);
+        
+        $("#otherWineImage2").empty();
+        var img = $("<img>");
+        img.attr("src", response.recommendedWines[1].imageUrl);
+        $("#otherWineImage2").append(img);
+        // var img = $("<img>");
+        // img.attr("src", response.recommendedWines[2].imageUrl);
+        // $("#otherWineImage3").append(img);
+        var title = $("<p>");
+        title.text(response.recommendedWines[0].title)
+        $("#otherWineImage1").append(title);
+        var title = $("<p>");
+        title.text(response.recommendedWines[1].title)
+        $("#otherWineImage2").append(title);
+        // var title = $("<p>");
+        // title.text(response.recommendedWines[2].title)
+        // $("#otherWineImage3").append(title);
+      })
 
     });
 
-   
+
 
 
     var newFood = {
       newFood: textInput,
     }
-    database.ref().push(newFood);
+
+    database.ref("/" + zip).push(newFood);
     $(".autocomplete").val("");
+
   });
 
   // $(".autocomplete").keyup(function (event) {
@@ -168,7 +224,7 @@ $(function () {
   // })
 
   database.ref().on("child_added", function (childSnapshot, prevChildKey) {
-    console.log(childSnapshot.val());
+    // console.log(childSnapshot.val());
   });
 });
 
