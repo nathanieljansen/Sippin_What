3c30b622f028cbfd9dee3777d59523f77cfff11b
Carol Giblin <giblinCK@gmail.com>
2018-03-27 18:23:51 -0600

    ugh

--- assets/script/app.js
+++ assets/script/app.js
@@ -10,18 +10,6 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-var config = {
-  apiKey: "AIzaSyCEsZxf-oF64tfJK_saGz85n0TqBi1yHlA",
-  authDomain: "sippin-what.firebaseapp.com",
-  databaseURL: "https://sippin-what.firebaseio.com",
-  projectId: "sippin-what",
-  storageBucket: "sippin-what.appspot.com",
-  messagingSenderId: "175010578796"
-};
-firebase.initializeApp(config);
-
-var database = firebase.database();
-
 var zip = "";
 
 var map, infoWindow;
@@ -38,7 +26,6 @@ function initMap() {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-      console.log("its working", pos)
 
       $.ajax({
         url: "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + pos.lat + "," + pos.lng + "&sensor=true",
@@ -47,8 +34,8 @@ function initMap() {
         console.log('did it work ???', resultsBack.results[2].address_components[0].long_name);
         zip = resultsBack.results[2].address_components[0].long_name;
       })
-      
-      // var userPostion = {
+
+      // var userPosition = {
       //   userPosition: pos,
       // }
       // database.ref().push(userPosition)
@@ -105,16 +92,20 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 $(function () {
   $('.parallax').parallax();
 
-  function pageScroll() {
-    window.scrollBy(0, 450);
-    // scrolldelay = setTimeout(pageScroll, 7);
-  }
+  // function pageScroll() {
+  //   window.scrollBy(0, 1);
+  //   scrolldelay = setTimeout(pageScroll, 1);
+  // }
 
 
 
 
   $(".btn").click(function () {
     event.preventDefault();
+    $("#words").empty();
+    $("#image").empty();
+    $("#otherWineImage1").empty();
+    $("#otherWineImage2").empty();
     var textInput = $(".autocomplete").val().trim().toLowerCase();
     var wineQueryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + textInput + "&maxPrice=50";
     var wineAPI = {
@@ -129,22 +120,21 @@ $(function () {
     }
     $.ajax(wineAPI).then(function (response) {
       console.log(response)
-      
+
 
       if (response.status === "failure") {
         console.log(response.message)
-        $(".notValid").text("Sorry! " + response.message +". We are always trying to improve. Thanks for you help!")
+        $(".notValid").text("Sorry! " + response.message + ". We are always trying to improve. Thanks for you help!")
       }
       else if (response.pairingText === "") {
-        $(".notValid").text("Thanks for making us better! We didn't find a pairing for " + textInput + " but we are always trying to improve our app" )
+        $(".notValid").text("Thanks for making us better! We didn't find a pairing for " + textInput + " but we are always trying to improve our app")
       }
 
       else {
         $(".notValid").empty();
-        pageScroll();
+        // pageScroll();
         var otherWines = response.pairedWines[0];
 
-       $("#words").empty();
         var p = $("<p>");
         p.text(response.pairingText);
         $("#words").append(p);
@@ -153,8 +143,6 @@ $(function () {
         description.text(response.productMatches[0].description);
         $("#words").append(description);
 
-        $("#image").empty();
-        console.log("empty again")
         var img = $("<img>");
         img.attr("src", response.productMatches[0].imageUrl);
         $("#image").html(img);
@@ -178,12 +166,9 @@ $(function () {
       }
       $.ajax(comparableAPI).then(function (response) {
         console.log(response.recommendedWines)
-        $("#otherWineImage1").empty();
         var img = $("<img>");
         img.attr("src", response.recommendedWines[0].imageUrl);
         $("#otherWineImage1").append(img);
-        
-        $("#otherWineImage2").empty();
         var img = $("<img>");
         img.attr("src", response.recommendedWines[1].imageUrl);
         $("#otherWineImage2").append(img);
@@ -234,7 +219,7 @@ $(function () {
   // })
 
   database.ref().on("child_added", function (childSnapshot, prevChildKey) {
-    // console.log(childSnapshot.val());
+    console.log(childSnapshot.val());
   });
 });
 
